3baeddd966345f66924691b632718d7065c63fc2
Nabin1234 <nabinacharofficial@gmail.com>
2025-03-31 10:06:05 +0530

    Worked on api

--- src/App.tsx
+++ src/App.tsx
@@ -4,47 +4,53 @@ import "./App.css";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import axios from "axios";
+import { API_URL } from "./constants";
 
+ type TProductResponse = IProductObj[]
 
-export type TProductResponse = IProductObj[]
-
-export interface IProductObj {
+ interface IProductObj {
   id: number
   title: string
   price: number
   description: string
   category: string
   image: string
-  rating: IRating
+  rating: Rating
 }
 
-export interface IRating {
+ interface Rating {
   rate: number
   count: number
 }
 
-
 function App() {
   const [products, setProducts] = useState<TProductResponse>([]);
 
-  const fetchData = async () => {
-    axios
-      .get("https://fakestoreapi.com/products")
-      .then((res) => setProducts(res.data))
-      .catch((e) => console.log(e));
+
+
+  const fetchApi = async () => {
+    const res = await axios.get(API_URL);
+
+    setProducts(res.data);
   };
 
+  // When component mounts
   useEffect(() => {
-    fetchData();
-  }, []);
+    fetchApi();
 
-  console.log(products, "products");
+   
+  }, []);
 
   return (
     <>
       <Header />
       {products?.map((item) => (
-        <Sidebar image={item.image} name={item?.title} description={item?.description} rating={item.rating.rate} />
+        <Sidebar
+          image={item.image}
+          name={item?.title}
+          description={item?.description}
+          rating={item.rating.rate}
+        />
       ))}
     </>
   );
